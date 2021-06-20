44d1cfbaa49b20ac0989a87deafb4b2158863e66
yungestdev <yungestmonsterhahaha@gmail.com>
2021-06-20 21:51:54 +0200

    1log

---
+++ ErrorPage/error.js
@@ -0,0 +1,5 @@
+unknown = true
+if (unknown = true) {
+    console.error("Error 403")
+}
+console.warn('Error 403: This browser is an unknown browser, please come back with another browser');
---
+++ index.js
@@ -0,0 +1,59 @@
+console.log('%cHello there Welcome to GiokeyAI', `
+  background: white;
+  border: 3px solid red;
+  color: red;
+  font-size: 50px;
+  margin: 40px;
+  padding: 20px;
+`);
+var unknown = false
+function CheckBrowser() {
+
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    {
+        console.log('Currently using Opera');
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+        console.log('Currently using Chrome');
+        
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+        console.log('Currently using Safari');
+    }
+    else if(navigator.userAgent.indexOf("Brave") != -1)
+    {
+        console.log("Currently using Brave")
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+        console.log('Currently using Firefox');
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+        console.log('Currently using IE'); 
+    }  
+    else 
+    {
+        unknown = true
+        if (unknown = true) {
+            window.location.replace("ErrorPage/Error403.html");
+        }
+        
+    }
+}
+var errorBase = false
+CheckBrowser()
+
+    while (errorBase = false) {
+        if (errorBase == true) {
+            errorBase = true
+            err = "code 200";
+            console.warn(`${err}`)
+
+            errorcd = "error dHJvbGxlZA=="
+
+            console.error(`${errorcd}`)
+        }
+    }
